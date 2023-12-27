b1a8e26339f6f587cfd2c3417d6eab1be9d5b711
RuttyJ <ruttyj92@hotmail.com>
2023-12-27 13:23:17 -0500

    Update PlayDealGame.js

--- server/PlayDeal/src/PlayDealGame.js
+++ server/PlayDeal/src/PlayDealGame.js
@@ -15,7 +15,7 @@ const Transfer = require("./turn/request/Transfer");
 /**
  * =====================================================
  *
- *                     PlayDealGame Game
+ *                     PlayDeal Game
  *
  * =====================================================
  */
